e02510348f30d34fb6e88ece2557d9a4539ca02f
SolatK <kacnanov.petya@mail.ru>
2025-04-22 12:11:35 +0300

    task 1

---
+++ src/main/java/tasks/Tasks.java
@@ -0,0 +1,10 @@
+package tasks;
+
+public class Tasks {
+    public static void task1() {
+        float b = 10;
+        float c = 30;
+        float a = 4 * (b + c - 1) / 2;
+        System.out.println(a);
+    }
+}
