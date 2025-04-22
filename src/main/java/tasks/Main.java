48f0e93ba1b568019e42293061526b2b7d4434b2
SolatK <kacnanov.petya@mail.ru>
2025-04-22 12:30:35 +0300

    more tasks done

--- src/main/java/tasks/Main.java
+++ src/main/java/tasks/Main.java
@@ -3,5 +3,9 @@ package tasks;
 public class Main {
     public static void main(String[] args) {
         Tasks.task1();
+        Tasks.task2(26);
+        Tasks.task3(126);
+        Tasks.task4(126.344F);
+        Tasks.task5(36559, 53);
     }
 }
--- src/main/java/tasks/Tasks.java
+++ src/main/java/tasks/Tasks.java
@@ -7,4 +7,33 @@ public class Tasks {
         float a = 4 * (b + c - 1) / 2;
         System.out.println(a);
     }
+
+    public static void task2(int n) {
+        int result = 0;
+        while (n > 0) {
+            result += n % 10;
+            n = n / 10;
+        }
+        System.out.println(result);
+    }
+
+    public static void task3(int n) {
+        int result = 0;
+        while (n > 0) {
+            result += n % 10;
+            n = n / 10;
+        }
+        System.out.println(result);
+    }
+
+    public static void task4(float n) {
+        int result = (int) (n - (n % 1));
+        System.out.println(result);
+    }
+
+    public static void task5(int q, int w) {
+        int result = q / w;
+        int left = q % w;
+        System.out.println(String.format("Результат %d, остаток %d", result, left));
+    }
 }
