d51b43965722ed6a541b41c907354b8e20ce040a
SolatK <kacnanov.petya@mail.ru>
2025-05-13 19:20:02 +0300

    забыл

--- src/main/java/tasks/Main.java
+++ src/main/java/tasks/Main.java
@@ -1,11 +1,8 @@
 package tasks;
 
-import tasks.homework6.Geometry;
-import tasks.homework6.Hospital;
-
 public class Main {
     public static void main(String[] args) {
-        Hospital.runClinic();
-        Geometry.runTest();
+
     }
 }
+
